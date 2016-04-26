4e1756894e2dad298f2d9a9defb8ad9f09093229
lukemorse <luke.morse@gmail.com>
2016-04-26 09:36:16 -0400

    5 day forcast working

--- WeatherApp/AViewController.swift
+++ WeatherApp/AViewController.swift
@@ -28,33 +28,61 @@ class AViewController: UIViewController, PageDelegate {
     
     @IBOutlet weak var tempLbl: UILabel!
     
+    @IBOutlet weak var highLbl: UILabel!
+    
+    @IBOutlet weak var lowLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         Weather().downloadCurrentWeather { (weather: Dictionary<String, AnyObject>) in
             
+            var tempMax = -1000.0
+            var tempMin = 1000.0
+            
             if let list: [AnyObject] = weather["list"] as? [AnyObject] {
-                if let today: Dictionary<String, AnyObject> = list[0] as? Dictionary<String, AnyObject> {
-                    if let main: Dictionary<String, Double> = today["main"] as? Dictionary<String, Double> {
-//                        print(main)
-                        let tempF = round((main["temp"]! * 9/5 - 459.67) * 10) / 10
-                        self.tempLbl.text = "\(tempF)º F"
-                    }
-                    if let weather: [AnyObject] = today["weather"] as? [AnyObject] {
-                        if let weatherDesc: Dictionary<String, AnyObject> = weather[0] as? Dictionary<String, AnyObject> {
-                            if let descString: String = weatherDesc["description"] as? String {
-                                self.weatherImg.image = UIImage(named: self.picDict[descString]!)
+                
+                for x in list[0...7] {
+                    if let main = x["main"] as! Dictionary<String, Double>? {
+                        
+                        //                        FIGURE OUT TEMP MIN
+                        if let tempMinTest = main["temp_min"] {
+                            if tempMin > tempMinTest {
+                                tempMin  = tempMinTest
+                                let tempF = round((tempMin * 9/5 - 459.67) * 10) / 10
+                                self.lowLbl.text = "Low: " + String(tempF) + "º F"
+                            }
+                        }
+                        
+                        
+                        //                        FIGURE OUT TEMP MAX
+                        if let tempMaxTest = main["temp_max"] {
+                            if tempMax < tempMaxTest {
+                                tempMax = tempMaxTest
+                                let tempF = round((tempMax * 9/5 - 459.67) * 10) / 10
+                                self.highLbl.text = "High: " + String(tempF) + "º F"
                             }
                         }
                     }
                 }
                 
+                
+                if let currentWeatherAndImage: Dictionary<String, AnyObject> = list[0] as? Dictionary<String, AnyObject> {
+                    if let main: Dictionary<String, Double> = currentWeatherAndImage["main"] as? Dictionary<String, Double> {
+                        let tempF = round((main["temp"]! * 9/5 - 459.67) * 10) / 10
+                        self.tempLbl.text = "\(tempF)º F"
+                    }
+                    if let weather: [AnyObject] = currentWeatherAndImage["weather"] as? [AnyObject] {
+                        if let weatherDesc: Dictionary<String, AnyObject> = weather[0] as? Dictionary<String, AnyObject> {
+                            if let descString: String = weatherDesc["description"] as? String {
+                                self.weatherImg.image = UIImage(named: self.picDict[descString]!)
+                                
+                            }
+                        }
+                    }
+                }
             }
         }
-        
     }
-    
-    
-    
     
 }
